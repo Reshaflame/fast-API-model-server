1c9852d98b97dd0971a7a57728b830d005517692
Reshaflame <reshaflame@gmail.com>
2025-05-31 12:52:38 +0300

    Updated requesting IP address and send it to Redis

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from app.endpoints.inference import router as inference_router
 import os
 import redis
+import requests
+
 
 app = FastAPI()
 app.include_router(inference_router, prefix="/api")
@@ -13,3 +15,21 @@ if not redis_url:
 
 redis_client = redis.Redis.from_url(redis_url)
 print(f"🔑 Redis URL loaded successfully.")  # Avoid printing sensitive content
+
+# app/main.py (after redis_client is initialized)
+REDIS_API_KEY = "TrueDetect:API_ENDPOINT"
+REDIS_RETRAIN_KEY = "TrueDetect:RETRAIN_ENDPOINT"
+
+try:
+    ip = requests.get("https://api.ipify.org").text.strip()
+    api_ip = f"http://{ip}"
+except Exception as e:
+    print(f"❌ Failed to get public IP: {e}")
+    api_ip = "http://127.0.0.1"
+api_predict = f"{api_ip}/api/predict"
+api_retrain = f"{api_ip}/api/retrain"
+redis_client.set(REDIS_API_KEY, api_predict)
+redis_client.set(REDIS_RETRAIN_KEY, api_retrain)
+
+
+print(f"📡 Registered endpoints: {api_predict}, {api_retrain}")
