c9e5f959242a24878182b0d0bc32ff1c863c1477
Reshaflame <reshaflame@gmail.com>
2025-06-05 20:23:05 +0300

    fixed time bug

--- app/utils/preprocess_utils.py
+++ app/utils/preprocess_utils.py
@@ -18,6 +18,7 @@ def preprocess_batch(json_data: list[dict], expected_features_path="data/expecte
     # Convert time column to integer seconds if needed
     if df["time"].dtype == object:
         df["time"] = pd.to_datetime(df["time"], errors="coerce")
+        df["time"] = df["time"].fillna(pd.Timestamp(0))
         df["time"] = (df["time"].astype("int64") // 1e9).astype(int)
 
     # Frequency-encode
